3fc2c3c6eddfbcca072519d1f49f7947a8eaea65
CharmonePinheiro <charmonepinheirorodrigues@gmail.com>
2022-12-28 11:17:51 -0300

    Validação se os campos estão preenchidos

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ const altura =document.getElementById("altura").value;
 const peso =document.getElementById("peso").value;
 const resultado =document.getElementById("resultado");
 
+// Validação se os campos estão preenchidos//
+
+if( ){
+    
+    
+}
 
 
 }
